3899087f66e21c0d3cb37ef02a2a4f7e3713b2a7
Csaba Agosthazy <40959918+csabaagosthazy@users.noreply.github.com>
2025-09-14 12:24:54 +0200

    Sections update
    Titles and slug are required
    Added descriptions

--- schemaTypes/section.ts
+++ schemaTypes/section.ts
@@ -9,21 +9,25 @@ export default defineType({
         defineField({
             name: "title",
             title: "Title",
-            type: "localeString", // so you can translate section names
+            type: "localeString",
+            validation: (Rule) => Rule.required(),
         }),
         defineField({
             name: "order",
             title: "Order",
             type: "number",
+            description: "Defines the order of the sections on the webpage"
         }),
         defineField({
             name: "slug",
             title: "Slug",
             type: "slug",
+            description: 'Press "generate" to create from title',
             options: {
                 source: "title.hu",
                 maxLength: 96,
             },
+            validation: (Rule) => Rule.required(),
         }),
     ],
     preview: {
